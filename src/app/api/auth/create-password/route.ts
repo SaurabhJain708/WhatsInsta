520eda8f6b94a749ab0fd0dbb63d1fc80caec44b
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-05-09 02:08:38 +0530

    Added create-pasword route

---
+++ src/app/api/auth/create-password/route.ts
@@ -0,0 +1,40 @@
+import { ApiError } from "@/lib/ApiError";
+import { ApiResponse } from "@/lib/ApiResponse";
+import { CheckAuth } from "@/lib/checkAuth";
+import { mongoDb } from "@/lib/mongodb";
+import { setAuthCookies } from "@/lib/resetCookies";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { password } = await req.json();
+    if (!password) {
+      return NextResponse.json(new ApiError(400, "Password is required"), {
+        status: 400,
+      });
+    }
+    await mongoDb();
+    const AuthContents = await CheckAuth(req);
+    if (!AuthContents || typeof AuthContents === "boolean") {
+      return NextResponse.json(new ApiError(403, "Authentication Failed"), {
+        status: 403,
+      });
+    }
+    const user = AuthContents.user;
+    user.areDetailsComplete = true;
+    user.password = password;
+    await user.save();
+
+    const response = NextResponse.json(
+      new ApiResponse(200, null, "Password updated successfully"),
+      { status: 200 }
+    );
+    setAuthCookies(response, AuthContents);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(new ApiError(500, `Internal server error`), {
+      status: 500,
+    });
+  }
+}
