631300d8e0698651bd98807970db86cfdb1c35fb
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-05-09 15:33:00 +0530

    Added credentials-login route

---
+++ src/app/api/auth/credentials-login/route.ts
@@ -0,0 +1,63 @@
+import { ApiError } from "@/lib/ApiError";
+import { ApiResponse } from "@/lib/ApiResponse";
+import { GenerateTokens } from "@/lib/generateAccess&RefreshToken";
+import { mongoDb } from "@/lib/mongodb";
+import { User } from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { password, email } = await req.json();
+    if (!password || !email) {
+      return NextResponse.json(
+        new ApiError(400, "Password and email are required"),
+        { status: 400 }
+      );
+    }
+    await mongoDb();
+    const findExistingUser = await User.findOne({ email });
+    if (!findExistingUser) {
+      return NextResponse.json(
+        new ApiError(404, "User not found please sign-up"),
+        { status: 404 }
+      );
+    }
+    const isPasswordCorrect = await findExistingUser.comparePassword(password);
+    if (!isPasswordCorrect) {
+      return NextResponse.json(new ApiError(401, "Incorrect password"), {
+        status: 401,
+      });
+    }
+    const token = await GenerateTokens(findExistingUser._id.toString());
+    if (!token || typeof token === "boolean") {
+      return NextResponse.json(
+        new ApiError(500, "Server error while generating tokens")
+      );
+    }
+    const response = NextResponse.json(
+      new ApiResponse(200, null, "User login successful"),
+      { status: 200 }
+    );
+    response.cookies.set("accessToken", token.accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Only secure in production
+      sameSite: "strict", // Adjust to your needs, 'strict' is safer
+      path: "/", // Accessible throughout the entire app
+      maxAge: 60 * 60,
+    });
+
+    response.cookies.set("refreshToken", token.refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Only secure in production
+      sameSite: "strict",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    });
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(new ApiError(500, `Server error`), {
+      status: 500,
+    });
+  }
+}
